ae2f7b76de36fddbd47d0541cfc3cd91726913a5
ariel <arieldiaz05@gmail.com>
2023-09-11 20:12:53 +0200

    Creacion Clase MvcConfig

---
+++ src/main/java/com/example/security/config/MvcConfig.java
@@ -0,0 +1,29 @@
+package com.example.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/403").setViewName("403");
+
+	}
+
+//	@Bean
+//	public ViewResolver viewResolver() {
+//		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+//		viewResolver.setPrefix("/templates/");
+//		viewResolver.setSuffix(".html");
+//		return viewResolver;
+//	}
+
+}
